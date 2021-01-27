d5520de97ad8b8d93a74a2d6ecd7b428d1cf1172
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2021-01-27 20:51:26 -0300

    fix config

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // Array of Access_tokens (strings) of your MercadoPago Accounts
-let access_token = ["APP-XXXXXXXXX-XXXXXXXXX-XXXXXXXXXX"];
+let access_token = ["APP-XXXXXXXXX-XXXXXXXXX-XXXXXXXXX-XXXXXXXXX"];
 
 //CHART & API parameters
 //WARNING: If your rollUp is small and your chartWindows is really big. You will have problems in watching all data into chart.
-//Best combinations: 15min --> (30000, 30) OR 30min --> (60000, 30) OR 60min --> (120000, 30) OR 120min -->(300000, 24)
+//Best combinations: 30min --> (30000, 60) OR 60min --> (30000, 120) OR 120min --> (60000, 120)
 
 let rollUp = 30000; // Time Windows between metrics or APIcalls in milisecs
-let chartWindows = 30; //Length of dataArray that shows in Chart - (chartWindows * rollUp) = Amount of data in chart
-//Example --> 30000ms * 30 = 900secs or 15 min that chart will show from begining to end
+let chartWindows = 120; //Length of dataArray that is shown in Chart - (chartWindows * rollUp) = Amount of data in chart
+//Example --> 30000ms * 120 = 3600secs or last 60min that chart will show from begining to end
 
 module.exports.access_token = access_token;
 module.exports.rollUp = rollUp;
